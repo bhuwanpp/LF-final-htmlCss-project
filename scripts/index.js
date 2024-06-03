e29dddcb0a9b58f6b00c668cd4f4675fb21f5598
Bhuwan <bhubanpaudel79@gmail.com>
2024-06-03 16:11:15 +0545

    finish project add shadow in div

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,11 @@ const themeBtn = document.getElementById('theme-btn')
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle("dark")
 })
+const menuBtn = document.getElementById('menu')
+const ulMenu = document.getElementById('ulMenu')
+
+menuBtn.addEventListener('click', () => {
+    console.log('click')
+    ulMenu.classList.toggle("hide")
+    console.log(ulMenu)
+})
